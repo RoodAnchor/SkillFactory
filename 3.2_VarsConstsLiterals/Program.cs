1d2f03ced1b5bf2243bb01bdb6bb27ee5ce6f990
RoodAnchor <ujcn2086@gmail.com>
2023-11-09 21:19:02 +0300

    Рассмотрел литералы

--- 3.2_VarsConstsLiterals/Program.cs
+++ 3.2_VarsConstsLiterals/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,45 @@ namespace SkillFactory.VarsConstsLiterals
     {
         static void Main(string[] args)
         {
-            String MyName; 
-            MyName = "Andrey";
+            PrintHexDecDouble();
+        }
+
+        private static void PrintStuffWithEscChars()
+        {
+            const String MyName = "Андрей";
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\tПривет, мир!");
+            Console.WriteLine("\tМне 36");
+            Console.WriteLine($"\tМеня зовут\n {MyName}");
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
+
+            Console.ReadKey();
+        }
+
+        private static void PrintLogicalLiterals()
+        {
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintNumericLiterals()
+        {
+            Console.WriteLine(5);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintHexDecDouble()
+        {
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
+
+            Console.ReadKey();
         }
     }
 }
