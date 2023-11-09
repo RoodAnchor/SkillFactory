5c93c9a492e4a4e6da160601671349bee3c1b460
RoodAnchor <ujcn2086@gmail.com>
2023-11-09 22:37:55 +0300

    Добавил практические задания по основным оперциям.

---
+++ 3.6_MainOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.MainOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IncrementPosfixPrint(2);
+            Console.ReadKey();
+        }
+
+        private static void DivideAndPrint(Double a, Double b)
+        {
+            Double result = a / b;
+
+            Console.WriteLine($"{a} / {b} = {result}");
+            
+        }
+
+        private static void RemainderPrint(Double a, Double b)
+        {
+            Double result = a % b;
+
+            Console.WriteLine($"{a} % {b} = {result}");
+        }
+
+        private static void IncrementPrefixPrint(Int32 value)
+        {
+            Console.WriteLine($"Initial: {value}, Increment: {++value}");
+        }
+
+        private static void IncrementPosfixPrint(Int32 value)
+        {
+            Console.WriteLine($"Initial: {value}, Increment: {value++}");
+        }
+    }
+}
