5f41481a8d56535ac36e34d8fdcf859e2505a26e
RoodAnchor <ujcn2086@gmail.com>
2023-11-09 21:36:20 +0300

    Создал перечисление дней недели.

---
+++ 3.4_Enums/WeekDaysEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.Enums
+{
+    enum WeekDaysEnum : Byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
