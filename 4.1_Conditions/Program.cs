b5bbf67ebe6d02a8d6865113b9acda3c47c1996e
RoodAnchor <ujcn2086@gmail.com>
2023-11-16 19:01:58 +0300

    Юнит 4, задание 4.1.16

---
+++ 4.1_Conditions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory.Module4.Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет " +
+                "на англиском языке с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            Console.WriteLine($"Ваш любимый цвет {color}");
+            Console.ReadLine();
+        }
+    }
+}
